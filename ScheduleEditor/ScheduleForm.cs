ce9ed73805b74c88badc1eac1af04267562d4652
Амридин Тоджидинович < @DESKTOP-AIIQKQN>
2025-05-09 18:11:30 +0300

    Добавьте файлы проекта.

---
+++ ScheduleEditor/ScheduleForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ScheduleEditor
+{
+    public partial class ScheduleForm : Form
+    {
+        public ScheduleForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
